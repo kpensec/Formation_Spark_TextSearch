dd3f2b0e07d81ca7aa39400a23f7f69dca3f7778
kpensec <kevin.pensec@gmail.com>
2018-03-18 15:53:37 +0100

    scala port :tada:

--- src/main/scala/example/TextSearch.scala
+++ src/main/scala/example/TextSearch.scala
@@ -1,40 +1,92 @@
 package formation
-import org.scalactic.TimesOnInt._
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-//import org.apache.spark.
 
-object TextSearch extends Greeting with App {
-  def extractWords(line: String) : String = {
-    return line
+import org.apache.spark.sql.SparkSession
+import scala.io.StdIn.readLine
+
+object TextSearch extends App {
+
+  def stripAll(bad: String) = {
+    (word : String) => {
+    import scala.annotation.tailrec
+      @tailrec def start(n: Int): String = 
+        if (n == word.length) ""
+        else if (bad.indexOf(word.charAt(n)) < 0) end(n, word.length)
+        else start(1 + n)
+
+      @tailrec def end(a: Int, n: Int): String =
+        if (n <= a) word.substring(a, n)
+        else if (bad.indexOf(word.charAt(n - 1)) < 0) word.substring(a, n)
+        else end(a, n - 1)
+
+      start(0)
+    }
   }
 
+  def extractWords(line: String) : Array[String] = line
+    .split(" ")
+    .map(stripAll("!?,.:;@#()[]{}$*'/\\-|"))
+    .filter(_!="")
+
+  case class WordCountData (
+    word: String,
+    count: BigInt,
+    length: Int
+  )
+
+  val QUERY_MOST_FREQUENT_NSIZED_WORD =
+  """ SELECT *
+    | FROM WordCountData
+    | WHERE length = %d
+    | ORDER BY count DESC
+    | LIMIT 10
+  """.stripMargin
   
+  val QUERY_LONGEST_MOST_FREQUENT_WORD =
+  """ SELECT *
+    | FROM WordCountData
+    | ORDER BY length DESC, count DESC
+    | LIMIT 10
+  """.stripMargin
+
+  def prepareForDataSet(kv: (String, BigInt)) = WordCountData(kv._1, kv._2, kv._1.length())
+  
+  def usage() {
+    println("run <inputURI>")
+  }
+
   for (e <- args) {
     println(e)
   }
   if (args.length <= 0) {
+    usage()
     sys.exit(-1)
   }
   val inputDirname = args(0);
-
-  // 10 times println(greeting)
-  val sparkContext = new SparkContext("local[1]", "Scala Text Search")
-  val textFile = sparkContext.textFile(inputDirname)
-  val rdd = textFile
-    .flatMap(extractWords)
-    .map(w => (w,1))
-    .reduceByKey(_ + _)
   
-      
-  rdd.collect().foreach(println)
+  val sparkSession = SparkSession.builder
+    .appName("Scala/Spark Text Search")
+    .master("local[1]")
+    .getOrCreate()
 
-  println("Press return to quit")
-  readLine();
-  println("jobs done!")
-  sparkContext.stop()
-  
-}
-trait Greeting {
-  lazy val greeting = "hello world!"
+  try {
+    import sparkSession.implicits._
+    sparkSession.sparkContext.setLogLevel("ERROR")
+    val lines = sparkSession.sqlContext.read.text(inputDirname).as[String]
+    val words = lines.flatMap(extractWords)
+    val wordCount = words.groupBy("value").count().as[(String, BigInt)]
+    val ds = wordCount.map(prepareForDataSet)
+    ds.cache().createTempView("WordCountData")
+
+    sparkSession.sql(QUERY_MOST_FREQUENT_NSIZED_WORD.format(4)).show()
+    sparkSession.sql(QUERY_MOST_FREQUENT_NSIZED_WORD.format(15)).show()
+    sparkSession.sql(QUERY_LONGEST_MOST_FREQUENT_WORD).show()
+  } catch {
+    case e: Exception => {
+      println("exception caught " + e )
+      e.printStackTrace()
+    }
+  } finally {
+    sparkSession.close()
+  }
+  readLine("Press Return to quit...");
 }
